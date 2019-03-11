bf72d5d252350eb2aedad1dc85c512b8adaa420d
vigoss6666 <zaheeryakub@gmail.com>
2019-03-11 14:02:54 +0530

    New

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,11 +1,5 @@
 import { Request, Response } from "express";
 
-/**
- * GET /
- * Home page.
- */
 export let index = (req: Request, res: Response) => {
-  res.render("home", {
-    title: "Home"
-  });
+  res.send("Hello world"); 
 };
